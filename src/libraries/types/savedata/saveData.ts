8260b84eee7eeb80749b68cded681f227fc6bee7
Bambus Control <mojmir.majer@gmail.com>
2024-05-21 19:56:28 +0200

    Version `0.1.0`

--- src/libraries/types/savedata/saveData.ts
+++ src/libraries/types/savedata/saveData.ts
@@ -14,8 +14,8 @@ export interface SaveData extends Initializable {
  * ```^[0-9]+\\.[0-9]+\\.[0-9]+(?:-[A-Z]+)?$```
  */
 export type SaveDataVersion
-    = "0.1.0-NEXT"
+    = "0.1.0"
     // Update only if save data schema changed
     ;
 
-export const CURRENT_VERSION: SaveDataVersion = "0.1.0-NEXT";
+export const CURRENT_VERSION: SaveDataVersion = "0.1.0";
